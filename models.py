b5f6ef89553c99b97cd10f4cf3ad800966fe6f83
farhanzaidann <farhnzaidan@gmail.com>
2025-12-24 20:10:57 +0700

    change title

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ def get_db_connection():
         host='localhost',
         user='root',
         password='',
-        database='neotix_db',
+        database='FesTix_db',
         cursorclass=pymysql.cursors.DictCursor
     )
 
